ab00875e2b3a4a216e4fa70cc76fbad32a0f1b53
Sheppyi Laptop <hirschmann.tim@t-online.de>
2018-07-02 23:46:05 +0200

    laptop compat

--- PuzzleGame/Assets/scritps/GamePlay/BlockPlacer.cs
+++ PuzzleGame/Assets/scritps/GamePlay/BlockPlacer.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class BlockPlacer : MonoBehaviour
 {
-    public static int SelectedElement = 0 ;     //selected element determains which element is currently selected (duh
+    public static int SelectedElement = 0;     //selected element determains which element is currently selected (duh
     public GameObject Selection;
 
     private bool isForeGround = true;
     private float foregroundDepth = -8f;
     private float backgroundDepth = -4f;
     private bool inTabAnimation = false;
-    private float TabAnimationLenght = 0.15f;
+    private float TabAnimationLenght = 0.2f;      //transition lenght in seconds
     public static float currentDepth;
     private float remainingTime;
 
@@ -28,14 +28,14 @@ public class BlockPlacer : MonoBehaviour
     {
         //select element
         if (Input.GetKeyDown("3") && SelectedElement == 0)
-        {      
+        {
             SelectElement(3);
         }
 
 
 
         //change level
-        if (Input.GetKeyDown(KeyCode.Tab)  && !inTabAnimation)
+        if (Input.GetKeyDown(KeyCode.Tab) && !inTabAnimation)
         {
             remainingTime = TabAnimationLenght;
             inTabAnimation = true;
@@ -55,7 +55,7 @@ public class BlockPlacer : MonoBehaviour
             var modifier = (foregroundDepth - backgroundDepth) / TabAnimationLenght * Time.deltaTime;
             if (!isForeGround)
             {
-                Camera.main.transform.position -= new Vector3(0,0,modifier);
+                Camera.main.transform.position -= new Vector3(0, 0, modifier);
                 var scaleModifier = PlayFieldForeGround.transform.localScale.x - (modifier * 15);
                 scaleModifier = Mathf.Clamp(scaleModifier, 1, 63);
                 PlayFieldForeGround.transform.localScale = new Vector3(scaleModifier, scaleModifier, scaleModifier);
@@ -69,12 +69,13 @@ public class BlockPlacer : MonoBehaviour
             }
             remainingTime -= Time.deltaTime;
         }
-        if(remainingTime <= 0)
+        if (remainingTime <= 0)
         {
             inTabAnimation = false;
         }
     }
 
+    
     private void SelectElement(int ElementNumber)
     {
         SelectedElement = ElementNumber;    //apply correct elementNumber
